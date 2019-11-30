122373d451c5fe2f143b7e7856e172e9e9eeb70d
DJ Bowers <dj@dj-bowers.com>
2019-11-30 13:42:32 -0800

    Add particle effect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Particles from 'react-particles-js';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
@@ -6,9 +7,24 @@ import Rank from './components/Rank/Rank';
 import './App.css';
 import 'tachyons';
 
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 50,
+      density: {
+        enable: true,
+        value_area: 500
+      }
+    }
+  }
+}
+
 function App() {
   return (
     <div className="App">
+      <Particles
+        className="Particles"
+        params={particlesOptions} />
       <Navigation />
       <Logo />
       <Rank />
